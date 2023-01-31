cbcad48fef76092453cea80a397494f9dabf3727
PromzzyKoncepts <okechukwupromise638@gmail.com>
2023-01-31 14:33:17 +0100

    Create class Teacher

---
+++ Teacher.rb
@@ -0,0 +1,11 @@
+require './Person'
+
+class Teacher 
+  def initialize (name, age = "Unknown", parent_permission = true)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
