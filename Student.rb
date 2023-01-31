5ac30a9a7c31705b986e0d30c83f4181c5f5dcd7
PromzzyKoncepts <okechukwupromise638@gmail.com>
2023-01-31 14:33:02 +0100

    Create class Student

---
+++ Student.rb
@@ -0,0 +1,11 @@
+require './Person'
+
+class Student 
+  def initialize(name, age = "Unknown", parent_permission = true)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    puts "¯\(ツ)/¯"
+  end
+end
