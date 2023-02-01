1f27383a68df051355aa9ca8a4d42ea51a620675
PromzzyKoncepts <okechukwupromise638@gmail.com>
2023-02-01 22:29:03 +0100

    Create decorator

---
+++ trimmer_decorator.rb
@@ -0,0 +1,6 @@
+require './base_decorator'
+class TrimmerDecorator < BaseDecorator
+  def correct_name
+    @nameable.correct_name.size > 10 ? @nameable.correct_name[0..9] : @nameable.correct_name
+  end
+end
