192b8e0c04f2114332b3a80723ed5551122fcf82
PromzzyKoncepts <okechukwupromise638@gmail.com>
2023-02-02 12:18:16 +0100

    Add puts to display output in terminal

--- Person.rb
+++ Person.rb
@@ -37,6 +37,6 @@ end
 person = Person.new(22, 'maximilianus')
 person.correct_name
 capitalized_person = CapitalizeDecorator.new(person)
-capitalized_person.correct_name
+puts capitalized_person.correct_name
 capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-capitalized_trimmed_person.correct_name
+puts capitalized_trimmed_person.correct_name
