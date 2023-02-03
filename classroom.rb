9ab4618d1af0e1e1148222e480d567e7e767e67d
PromzzyKoncepts <okechukwupromise638@gmail.com>
2023-02-03 14:41:18 +0100

    Setting up associations in classroom class

---
+++ classroom.rb
@@ -0,0 +1,15 @@
+class classroom 
+  attr_accessor :label
+  attr_reader :students
+
+  def initialize(label)
+    @label = label
+    @students = []
+  end
+
+  def adding_students(students)
+    @students.push(students)
+    student.classroom << self
+  end
+  
+end
