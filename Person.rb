adaf0b8cdfaeb6e3274f940b4d341ad80578bc85
PromzzyKoncepts <okechukwupromise638@gmail.com>
2023-02-03 17:19:31 +0100

    make changes and correct linter errors

--- Person.rb
+++ Person.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require './nameable'
 require './base_decorator'
 require './capitalize_decorator'
--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require './base_decorator'
 class TrimmerDecorator < BaseDecorator
   def correct_name
