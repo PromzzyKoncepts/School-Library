d845e65bd7f731c640bdd102a926fbc5e1d2cd75
belalgamal46 <belalgamal00@gmail.com>
2023-02-06 15:18:35 +0200

    Modify code to run main.rb

--- persons.rb
+++ persons.rb
@@ -37,10 +37,3 @@ class Person < Nameable
     Rental.new(date, book, self)
   end
 end
-
-person = Person.new('maximilianus', 22)
-person.correct_name
-capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
